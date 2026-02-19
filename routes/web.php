de6146007ea763e48f19d7d9971ee791f58cf9ba
FONTAINE Alex <alex.m.fontaine@gmail.com>
2026-02-19 11:05:44 +0100

    feat(maryui) basic install

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,3 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
-
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
-require __DIR__.'/settings.php';
+Route::livewire('/', 'pages::users.index');
